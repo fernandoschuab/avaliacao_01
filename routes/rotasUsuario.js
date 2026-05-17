ad7526c2d0f2c7e5384434548ba55e85df51d80f
Fernando Schuab <fschuab@gmail.com>
2026-05-17 18:13:35 -0300

    fix: add authentication middleware to the logout route

--- routes/rotasUsuario.js
+++ routes/rotasUsuario.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controllerUsuario = require('../controller/controllerUsuario');
+const { ehAutenticado } = require('../middlewares/controleUsuario');
 
 router.get('/cadastro', controllerUsuario.cadastro_get);
 router.post('/cadastro', controllerUsuario.cadastro_post);
 router.get('/login', controllerUsuario.login_get);
 router.post('/login', controllerUsuario.login_post);
-router.post('/logout', controllerUsuario.logout);
+router.post('/logout', ehAutenticado, controllerUsuario.logout);
 
 module.exports = router;
